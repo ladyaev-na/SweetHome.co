fd2be6ea08022a0b7fa694b7dc8533b28bd6d3bd
ladyaev-na <LADYAYEV@INBOX.RU>
2024-03-05 18:06:49 +0700

    a

--- database/migrations/2024_03_05_111057_create_photos_table.php
+++ database/migrations/2024_03_05_111057_create_photos_table.php
@@ -6,18 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->dateTime('datetime')->default(now());
-            $table->string('address');
             $table->timestamps();
         });
     }
+
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('photos');
     }
 };
